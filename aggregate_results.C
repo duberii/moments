cce36dfe5d5560c6a22a4a86b7a0c5a68883c54a
Richard Dube <richardddube@gmail.com>
2026-09-24 11:41:09 -0400

    simplified aggregation and split up total uncertainty and statistical uncertainty from the data

--- aggregate_results.C
+++ aggregate_results.C
@@ -6,77 +6,178 @@ void aggregate_results(const char* dataset_name, int maxL, int num_bootstraps) {
     AnalysisInfo analysis_info = getAnalysisInfo(dataset_name);
     std::vector<TString> figure_names;
     std::unique_ptr<TCanvas> canvas = std::make_unique<TCanvas>("canvas","canvas",700,500);
+    int dimension = 3*(maxL+1)*(maxL+2)/2 - maxL-1;
+    MomentsArray moments_index_handler(maxL);
+
+    //===================================================
+    // Making fully bootstrapped figures
+    //===================================================
+
+    // First, read the nominal values from the "full" file
+
+    std::unique_ptr<TFile> main_file = std::unique_ptr<TFile>(TFile::Open(TString::Format("%s%s/%i/full/data/moments.root", analysis_info.output_path.Data(), dataset_name, maxL),"UPDATE"));
+    
+    std::vector<std::vector<double>> means = std::vector<std::vector<double>>(dimension,std::vector<double>(analysis_info.n_bins));
+    std::vector<std::vector<double>> means_raw = std::vector<std::vector<double>>(dimension,std::vector<double>(analysis_info.n_bins));
+    std::vector<std::vector<double>> means_acceptance_corrected = std::vector<std::vector<double>>(dimension,std::vector<double>(analysis_info.n_bins));
+    std::vector<std::vector<double>> means_raw_acceptance_corrected = std::vector<std::vector<double>>(dimension,std::vector<double>(analysis_info.n_bins));
+    std::cout<< "Beginning to extract means from main file" << std::endl;
     for (int alpha = 0; alpha <3; alpha++) {
         for (int L =0; L <= maxL; L++) {
             for (int M =0; M <= L; M++) { 
                 if (alpha==2 && M==0) {
                     continue;
                 }
-                figure_names.push_back(TString::Format("H_%i_%i_%i", alpha, L, M));
-                figure_names.push_back(TString::Format("H_%i_%i_%i_raw", alpha, L, M));
-                figure_names.push_back(TString::Format("H_%i_%i_%i_acceptance_corrected", alpha, L, M));
-                figure_names.push_back(TString::Format("H_%i_%i_%i_raw_acceptance_corrected", alpha, L, M));
+                int moment_number = moments_index_handler.getMomentIndex(alpha,L,M);
+                TH1D* moments_histogram = (TH1D*)main_file->Get(TString::Format("H_%i_%i_%i", alpha, L, M));
+                Double_t* moments_values = moments_histogram->GetArray();
+                TH1D* moments_histogram_raw = (TH1D*)main_file->Get(TString::Format("H_%i_%i_%i_raw", alpha, L, M));
+                Double_t* moments_values_raw = moments_histogram_raw->GetArray();
+                TH1D* moments_histogram_acceptance_corrected = (TH1D*)main_file->Get(TString::Format("H_%i_%i_%i_acceptance_corrected", alpha, L, M));
+                Double_t* moments_values_acceptance_corrected = moments_histogram_acceptance_corrected->GetArray();
+                TH1D* moments_histogram_raw_acceptance_corrected = (TH1D*)main_file->Get(TString::Format("H_%i_%i_%i_raw_acceptance_corrected", alpha, L, M));
+                Double_t* moments_values_raw_acceptance_corrected = moments_histogram_raw_acceptance_corrected->GetArray();
+                for (int bin = 1; bin <= analysis_info.n_bins; bin++) {
+                    means[moment_number][bin-1] = moments_values[bin];
+                    means_raw[moment_number][bin-1] = moments_values_raw[bin];
+                    means_acceptance_corrected[moment_number][bin-1] = moments_values_acceptance_corrected[bin];
+                    means_raw_acceptance_corrected[moment_number][bin-1] = moments_values_raw_acceptance_corrected[bin];
+                }
             }
         }
     }
-    std::vector<TString> data_files =  {"data","accMC","genMC"};
-    for (int data_file_number = 0; data_file_number < int(data_files.size()); data_file_number++) {
-        std::unique_ptr<TFile> main_file = std::unique_ptr<TFile>(TFile::Open(TString::Format("%s%s/%i/full/%s/moments.root", analysis_info.output_path.Data(), dataset_name, maxL, data_files[data_file_number].Data()),"UPDATE"));
-        std::vector<std::vector<double>> average(analysis_info.n_bins, std::vector<double>(figure_names.size()));
-        std::vector<std::vector<double>> square_average(analysis_info.n_bins, std::vector<double>(figure_names.size()));
-        std::vector<std::vector<double>> std_devs(analysis_info.n_bins, std::vector<double>(figure_names.size()));
-        std::vector<bool> figure_names_in_current_file(figure_names.size());
-        for (int figure_number = 0; figure_number < int(figure_names.size()); figure_number++) { 
-            TH1D* figure = dynamic_cast<TH1D*>(main_file->Get(figure_names[figure_number]));
-            figure_names_in_current_file[figure_number] = (figure != nullptr);
+    std::cout<< "Done extracting means from main file" << std::endl;
+
+    // Now find the std dev for all the bootstrapped figures
+    std::vector<std::vector<double>> std_devs = std::vector<std::vector<double>>(dimension,std::vector<double>(analysis_info.n_bins));
+    std::vector<std::vector<double>> std_devs_raw = std::vector<std::vector<double>>(dimension,std::vector<double>(analysis_info.n_bins));
+    std::vector<std::vector<double>> std_devs_acceptance_corrected = std::vector<std::vector<double>>(dimension,std::vector<double>(analysis_info.n_bins));
+    std::vector<std::vector<double>> std_devs_raw_acceptance_corrected = std::vector<std::vector<double>>(dimension,std::vector<double>(analysis_info.n_bins));
+    
+    std::cout<< "Beginning to calculate std dev from bootstrapped files" << std::endl;
+    for (int bootstrap_number = 0; bootstrap_number < num_bootstraps; bootstrap_number++) {
+        std::unique_ptr<TFile> bootstrap_file = std::unique_ptr<TFile>(TFile::Open(TString::Format("%s%s/%i/bootstraps/%i/data/moments.root", analysis_info.output_path.Data(), dataset_name, maxL, bootstrap_number),"READ"));
+        for (int alpha = 0; alpha <3; alpha++) {
+            for (int L =0; L <= maxL; L++) {
+                for (int M =0; M <= L; M++) { 
+                    if (alpha==2 && M==0) {
+                        continue;
+                    }
+                    int moment_number = moments_index_handler.getMomentIndex(alpha,L,M);
+                    TH1D* moments_histogram = (TH1D*)bootstrap_file->Get(TString::Format("H_%i_%i_%i", alpha, L, M));
+                    Double_t* moments_values = moments_histogram->GetArray();
+                    TH1D* moments_histogram_raw = (TH1D*)bootstrap_file->Get(TString::Format("H_%i_%i_%i_raw", alpha, L, M));
+                    Double_t* moments_values_raw = moments_histogram_raw->GetArray();
+                    TH1D* moments_histogram_acceptance_corrected = (TH1D*)bootstrap_file->Get(TString::Format("H_%i_%i_%i_acceptance_corrected", alpha, L, M));
+                    Double_t* moments_values_acceptance_corrected = moments_histogram_acceptance_corrected->GetArray();
+                    TH1D* moments_histogram_raw_acceptance_corrected = (TH1D*)bootstrap_file->Get(TString::Format("H_%i_%i_%i_raw_acceptance_corrected", alpha, L, M));
+                    Double_t* moments_values_raw_acceptance_corrected = moments_histogram_raw_acceptance_corrected->GetArray();
+                    for (int bin = 1; bin <= analysis_info.n_bins; bin++) {
+                        std_devs[moment_number][bin-1] += pow(moments_values[bin]-means[moment_number][bin-1],2);
+                        std_devs_raw[moment_number][bin-1] += pow(moments_values_raw[bin]-means_raw[moment_number][bin-1],2);
+                        std_devs_acceptance_corrected[moment_number][bin-1] += pow(moments_values_acceptance_corrected[bin]-means_acceptance_corrected[moment_number][bin-1],2);
+                        std_devs_raw_acceptance_corrected[moment_number][bin-1] += pow(moments_values_raw_acceptance_corrected[bin]-means_raw_acceptance_corrected[moment_number][bin-1],2);
+                    }
+                }
+            }
         }
-        for (int bootstrap_number = 0; bootstrap_number < num_bootstraps; bootstrap_number++) {
-            std::unique_ptr<TFile> bootstrap_file = std::unique_ptr<TFile>(TFile::Open(TString::Format("%s%s/%i/bootstraps/%i/%s/moments.root", analysis_info.output_path.Data(), dataset_name, maxL, bootstrap_number, data_files[data_file_number].Data()),"READ"));
-            for (int figure_number = 0; figure_number < int(figure_names_in_current_file.size()); figure_number++) {
-                if (!figure_names_in_current_file[figure_number]) {
+    }
+    std::cout<< "Finished calculating std dev from bootstrapped files" << std::endl;
+
+    //Actually make and save the new figures with the right uncertainties
+
+    main_file->cd();
+    for (int alpha = 0; alpha <3; alpha++) {
+        for (int L =0; L <= maxL; L++) {
+            for (int M =0; M <= L; M++) { 
+                if (alpha==2 && M==0) {
                     continue;
                 }
-                TH1D* bootstrap_figure = dynamic_cast<TH1D*>(bootstrap_file->Get(figure_names[figure_number]));
-                for (int bin=0; bin < analysis_info.n_bins; bin++) {
-                    double bin_val = bootstrap_figure->GetBinContent(bin+1);
-                    average[bin][figure_number] += bin_val;
-                    square_average[bin][figure_number] += bin_val*bin_val;
+                int moment_number = moments_index_handler.getMomentIndex(alpha,L,M);
+                TH1D* moments_histogram = (TH1D*)main_file->Get(TString::Format("H_%i_%i_%i", alpha, L, M));
+                TH1D* moments_histogram_raw = (TH1D*)main_file->Get(TString::Format("H_%i_%i_%i_raw", alpha, L, M));
+                TH1D* moments_histogram_acceptance_corrected = (TH1D*)main_file->Get(TString::Format("H_%i_%i_%i_acceptance_corrected", alpha, L, M));
+                TH1D* moments_histogram_raw_acceptance_corrected = (TH1D*)main_file->Get(TString::Format("H_%i_%i_%i_raw_acceptance_corrected", alpha, L, M));
+                for (int bin = 1; bin <= analysis_info.n_bins; bin++) {
+                    moments_histogram->SetBinError(bin,sqrt(std_devs[moment_number][bin-1]/num_bootstraps));
+                    moments_histogram_raw->SetBinError(bin,sqrt(std_devs_raw[moment_number][bin-1]/num_bootstraps));
+                    moments_histogram_acceptance_corrected->SetBinError(bin,sqrt(std_devs_acceptance_corrected[moment_number][bin-1]/num_bootstraps));
+                    moments_histogram_raw_acceptance_corrected->SetBinError(bin,sqrt(std_devs_raw_acceptance_corrected[moment_number][bin-1]/num_bootstraps));
                 }
+                moments_histogram->Write();
+                moments_histogram_raw->Write();
+                moments_histogram_acceptance_corrected->Write();
+                moments_histogram_raw_acceptance_corrected->Write();
             }
-            bootstrap_file->Close();
         }
-        
-        for (int figure_number = 0; figure_number < int(figure_names_in_current_file.size()); figure_number++) {
-            if (!figure_names_in_current_file[figure_number]) {
-                continue;
-            }
-            TH1D* figure = dynamic_cast<TH1D*>(main_file->Get(figure_names[figure_number]));
-            double y_min = 0.0;
-            double y_max = 0.0;
-            for (int bin=0; bin < analysis_info.n_bins; bin++) {
-                average[bin][figure_number] /= num_bootstraps;
-                square_average[bin][figure_number] /= num_bootstraps;
-                double diff = square_average[bin][figure_number] - average[bin][figure_number]*average[bin][figure_number];
-                if (diff <= 0) {
-                    std_devs[bin][figure_number] = 0.;
-                } else {
-                    std_devs[bin][figure_number] = sqrt(diff);
-                }
-                figure->SetBinError(bin+1, std_devs[bin][figure_number]);
-                y_min = min(figure->GetBinContent(bin+1)-std_devs[bin][figure_number], y_min);
-                y_max = max(figure->GetBinContent(bin+1)+std_devs[bin][figure_number], y_max);
-            }
-            figure->SetMaximum(1.1*y_max);
-            figure->SetMinimum(1.1*y_min);
-            main_file->cd();
-            figure->Write();
-            //canvas->cd();
-            //std::unique_ptr<TLine> zero_line = std::make_unique<TLine>(figure->GetXaxis()->GetXmin(),0,figure->GetXaxis()->GetXmax(),0);
-            //figure->Draw("E1");
-            //zero_line->Draw("SAME");
-            //canvas->SaveAs(TString::Format("%sall/moments/%s/%s.png", analysis_info.output_path, data_files[data_file_number].Data(),figure_names[figure_number].Data()));
-        }
-        main_file->Close();
     }
 
+    //===================================================
+    // Making statistical uncertainty figures
+    //===================================================
+
+    // Now find the std dev for all the bootstrapped figures
+    std::vector<std::vector<double>> std_devs_acceptance_corrected_statistical= std::vector<std::vector<double>>(dimension,std::vector<double>(analysis_info.n_bins));
+    std::vector<std::vector<double>> std_devs_raw_acceptance_corrected_statistical= std::vector<std::vector<double>>(dimension,std::vector<double>(analysis_info.n_bins));
+    std::cout<< "Beginning to calculate std dev (statstical error) from bootstrapped files" << std::endl;
+    std::vector<TMatrixD> inverse_acceptance_matrices;
+    inverse_acceptance_matrices.reserve(analysis_info.n_bins);
+    TFile* acceptance_matrix_fin = TFile::Open(TString::Format("%s%s/%i/full/accMC/acceptance_matrix.root", analysis_info.output_path.Data(), dataset_name, maxL),"READ");
+    for (int bin = 0; bin < analysis_info.n_bins; bin++) {
+        TMatrixD* mtx = (TMatrixD*)acceptance_matrix_fin->Get(TString::Format("acceptanceMatrixInverse_%i",bin));
+        inverse_acceptance_matrices.emplace_back(*mtx);
+    }
+    for (int bootstrap_number = 0; bootstrap_number < num_bootstraps; bootstrap_number++) {
+        std::unique_ptr<TFile> bootstrap_file = std::unique_ptr<TFile>(TFile::Open(TString::Format("%s%s/%i/bootstraps/%i/data/moments.root", analysis_info.output_path.Data(), dataset_name, maxL, bootstrap_number),"READ"));
+        std::vector<TMatrixD> moments_vectors_raw;
+        for (int bin = 0; bin < analysis_info.n_bins; bin++) {
+            moments_vectors_raw.emplace_back(dimension,1);
+        }
+        for (int alpha = 0; alpha <3; alpha++) {
+            for (int L =0; L <= maxL; L++) {
+                for (int M =0; M <= L; M++) { 
+                    if (alpha==2 && M==0) {
+                        continue;
+                    }
+                    int moment_number = moments_index_handler.getMomentIndex(alpha,L,M);
+                    TH1D* moments_histogram_raw = (TH1D*)bootstrap_file->Get(TString::Format("H_%i_%i_%i_raw", alpha, L, M));
+                    Double_t* moments_values_raw = moments_histogram_raw->GetArray();
+                    TH1D* moments_histogram = (TH1D*)bootstrap_file->Get(TString::Format("H_%i_%i_%i", alpha, L, M));
+                    Double_t* moments_values = moments_histogram->GetArray();
+                    for (int bin = 1; bin <= analysis_info.n_bins; bin++) {
+                        moments_vectors_raw[bin-1](moment_number,0) = moments_values_raw[bin];
+                    }
+                }
+            }
+        }
+        for (int bin = 1; bin <= analysis_info.n_bins; bin++) {
+            TMatrixD truth_moments_vector = inverse_acceptance_matrices[bin-1] * moments_vectors_raw[bin-1];
+            for (int moment_number = 0; moment_number < dimension; moment_number++) {
+                std_devs_raw_acceptance_corrected_statistical[moment_number][bin-1] += pow(truth_moments_vector(moment_number,0)-means_raw_acceptance_corrected[moment_number][bin-1],2);
+                std_devs_acceptance_corrected_statistical[moment_number][bin-1] += pow(truth_moments_vector(moment_number,0)/truth_moments_vector(0,0)-means_acceptance_corrected[moment_number][bin-1],2);
+            }
+        }
+    }
+
+    //Actually make and save the new figures with the right (statistical) uncertainties
+
+    main_file->cd();
+    for (int alpha = 0; alpha <3; alpha++) {
+        for (int L =0; L <= maxL; L++) {
+            for (int M =0; M <= L; M++) { 
+                if (alpha==2 && M==0) {
+                    continue;
+                }
+                int moment_number = moments_index_handler.getMomentIndex(alpha,L,M);
+                TH1D* moments_histogram_acceptance_corrected = (TH1D*)((TH1D*)main_file->Get(TString::Format("H_%i_%i_%i_acceptance_corrected", alpha, L, M)))->Clone(TString::Format("H_%i_%i_%i_acceptance_corrected_statistical", alpha, L, M));
+                TH1D* moments_histogram_raw_acceptance_corrected = (TH1D*)((TH1D*)main_file->Get(TString::Format("H_%i_%i_%i_raw_acceptance_corrected", alpha, L, M)))->Clone(TString::Format("H_%i_%i_%i_raw_acceptance_corrected_statistical", alpha, L, M));
+                for (int bin = 1; bin <= analysis_info.n_bins; bin++) {
+                    moments_histogram_acceptance_corrected->SetBinError(bin,sqrt(std_devs_acceptance_corrected_statistical[moment_number][bin-1]/num_bootstraps));
+                    moments_histogram_raw_acceptance_corrected->SetBinError(bin,sqrt(std_devs_raw_acceptance_corrected_statistical[moment_number][bin-1]/num_bootstraps));
+                }
+                moments_histogram_acceptance_corrected->Write();
+                moments_histogram_raw_acceptance_corrected->Write();
+            }
+        }
+    }
 }
