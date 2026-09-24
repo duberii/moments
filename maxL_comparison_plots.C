e8ca86f07a59fbe84d65d043d10e05198c2e6f2f
Richard Dube <richardddube@gmail.com>
2026-09-24 11:46:37 -0400

    Output figures split error bars into statistical uncertainty from data and uncertainty from MC

--- maxL_comparison_plots.C
+++ maxL_comparison_plots.C
@@ -6,6 +6,7 @@ void maxL_comparison_plots(const char* dataset_name, std::vector<int> maxLs_to_c
     std::vector<std::unique_ptr<TFile>> input_files;
     std::vector<TString> labels;
     int largest_L = 0;
+    std::cout << "Opening data files" << std::endl;
     for (int maxL : maxLs_to_compare) {
         input_files.emplace_back(TFile::Open(TString::Format("%s%s/%i/full/data/moments.root", analysis_info.output_path.Data(), dataset_name, maxL),"UPDATE"));
         labels.push_back(TString::Format("L_{max}=%i",maxL));
@@ -13,9 +14,10 @@ void maxL_comparison_plots(const char* dataset_name, std::vector<int> maxLs_to_c
             largest_L = maxL;
         }
     }
+    std::cout << "Done opening data files" << std::endl;
     std::unique_ptr<TCanvas> canvas = std::make_unique<TCanvas>("temp_canv","temp_canv",700,500);
     //compare acceptance corrected moments between maxLs
-    TString output_path = TString::Format("%s%s/",analysis_info.output_path.Data(),dataset_name);
+    TString output_path = TString::Format("%s%s/maxL_comparison_plots/",analysis_info.output_path.Data(),dataset_name);
     for (int alpha = 0; alpha <3; alpha++) {
         for (int L =0; L <= largest_L; L++) {
             for (int M =0; M <= L; M++) { 
@@ -23,25 +25,51 @@ void maxL_comparison_plots(const char* dataset_name, std::vector<int> maxLs_to_c
                     continue;
                 }
                 std::vector<TH1D*> histograms;
+                std::vector<TH1D*> histograms_raw;
+                std::vector<TH1D*> histograms_statistical;
+                std::vector<TH1D*> histograms_raw_statistical;
                 std::vector<TString> labels_to_use;
-                TString fig_name_in;
-                TString fig_name_out;
-                if (alpha == 0 && L == 0 && M == 0) {
-                    fig_name_in = TString::Format("H_%i_%i_%i_raw_acceptance_corrected", alpha, L, M);
-                    fig_name_out = TString::Format("H_%i_%i_%i_raw", alpha, L, M);
-                } else {
-                    fig_name_in = TString::Format("H_%i_%i_%i_acceptance_corrected", alpha, L, M);
-                    fig_name_out = TString::Format("H_%i_%i_%i", alpha, L, M);
+                TString fig_name_in_raw = TString::Format("H_%i_%i_%i_raw_acceptance_corrected", alpha, L, M);
+                TString fig_name_out_raw = TString::Format("H_%i_%i_%i_raw", alpha, L, M);
+                TString fig_name_in = TString::Format("H_%i_%i_%i_acceptance_corrected", alpha, L, M);
+                TString fig_name_out = TString::Format("H_%i_%i_%i", alpha, L, M);
+
+                //Only raw
+                std::cout << "Making alpha=" << alpha << " L=" << L << " M=" << M << " raw histograms"<< std::endl;
+                for (unsigned int i=0; i < maxLs_to_compare.size(); i++) {
+                    if (L <= maxLs_to_compare[i]) {
+                        TH1D* hist_raw = (TH1D*)input_files[i]->Get(fig_name_in_raw);
+                        TH1D* hist_raw_statistical = (TH1D*)input_files[i]->Get(fig_name_in_raw + "_statistical");
+                        hist_raw->SetDirectory(nullptr);
+                        hist_raw_statistical->SetDirectory(nullptr);
+                        histograms_raw.push_back(hist_raw);
+                        histograms_raw_statistical.push_back(hist_raw_statistical);
+                        labels_to_use.push_back(labels[i]);
+                    }
                 }
+                draw_on_same_canvas_with_statistical_errors(histograms_raw, histograms_raw_statistical,labels_to_use,fig_name_out_raw, output_file.get(), canvas.get(), output_path); 
+                std::cout << "Done making alpha=" << alpha << " L=" << L << " M=" << M << " raw histograms"<< std::endl;
+                if (alpha == 0 && L == 0 && M == 0) {
+                    continue;
+                }
+
+                std::cout << "Making alpha=" << alpha << " L=" << L << " M=" << M << " histograms"<< std::endl;
+
+                //Only normalized
                 for (unsigned int i=0; i < maxLs_to_compare.size(); i++) {
                     if (L <= maxLs_to_compare[i]) {
                         TH1D* hist = (TH1D*)input_files[i]->Get(fig_name_in);
+                        TH1D* hist_statistical = (TH1D*)input_files[i]->Get(fig_name_in + "_statistical");
                         hist->SetDirectory(nullptr);
+                        hist_statistical->SetDirectory(nullptr);
                         histograms.push_back(hist);
+                        histograms_statistical.push_back(hist_statistical);
                         labels_to_use.push_back(labels[i]);
-                    }                    
+                    }
                 }
-                draw_on_same_canvas(histograms,labels_to_use,fig_name_out,output_file.get(),canvas.get(), output_path);                
+                draw_on_same_canvas_with_statistical_errors(histograms, histograms_statistical,labels_to_use,fig_name_out, output_file.get(), canvas.get(), output_path); 
+                        
+                std::cout << "Done making alpha=" << alpha << " L=" << L << " M=" << M << " histograms"<< std::endl;
             }
         }
     }
